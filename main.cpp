c91a85ac3d6ab0b50ea21468c0af9843d5da614e
Fikri Syahid <47740296+fikrisyahid19@users.noreply.github.com>
2020-10-21 17:19:34 +0700

    Big Update
    - Mulai dari update ini akan mengikutin manual cara penggunaan atm mandiri
    - Penambahan suara ketika tombol ditekan
    - Perubahan tampilan seluruh menu
    - Perubahan ukuran window size dan screen buffer

--- main.cpp
+++ main.cpp
@@ -20,58 +20,29 @@ using namespace std;
 // =======================================================================================
 
 HANDLE out	= GetStdHandle(STD_OUTPUT_HANDLE);
-HANDLE in	= GetStdHandle(STD_INPUT_HANDLE);
 HWND wh 	= GetConsoleWindow();
 
-void fontsize(int,int);
-void showHeader();
 void masukkanKartu();
-void masukkanPin(string&);
+void masukkanPin(string);
 void mainMenu(char&);
+void keluarKartu();
+void configureCMD();
+void fontsize(int,int);
 
 int main(){
-	// Data nasabah ==============================================
+	// Variabel ==================================================
 	char pilihan = 0;												// Variabel sementara untuk pilihan pada main menu
-	string pin	= "";												// Variabel sementara untuk pin
+	
+	// Data nasabah ==============================================
 	string pinT	= "123456";											// PIN untuk akses ATM
 	int saldo	= 10000000;											// Jumlah saldo
-	// ===========================================================
 	
 	// Pengaturan CMD ============================================
-	_COORD coord;													// Variabel untuk menyimpan window size
-	coord.X = 47; 
-	coord.Y = 16;
-	_SMALL_RECT Rect;												// Variabel untuk menyimpan screen buffer size
-    Rect.Top = Rect.Left = 0;
-    Rect.Right = 46;
-	Rect.Bottom = 15;
-	SetConsoleWindowInfo(out, TRUE, &Rect);							// Merubah ukuran window size
-	SetConsoleScreenBufferSize(out, coord);							// Merubah ukuran screen buffer size
-	system("title Simulasi Mesin ATM v1.0");						// Ubah title CMD
-	fontsize(35, 35);												// Ubah ukuran font pada CMD
-    system("color 17");												// Merubah warna background CMD menjadi biru & warna teks menjadi putih
-    SetWindowLong(wh, GWL_STYLE,									// Disable resize dan maximize pada window CMD 
-				  GetWindowLong(wh, GWL_STYLE) 
-				  & ~WS_MAXIMIZEBOX & ~WS_SIZEBOX);
-    // ===========================================================
+	configureCMD();													// Mengatur tampilan CMD
 
     // Kartu dan PIN =============================================
     masukkanKartu();												// Tampilan masukkan kartu
-    while(pin != pinT){												// Ketika pin tidak sama dengan pinT
-    	pin = "";													// maka akan terus menampilkan tampilan masukkan pin
-    	masukkanPin(pin);
-    	if(pin != pinT){
-    		std::cout << "\n\n\n\t  Pin yang anda masukkan salah\n";
-    		sleep(4);
-		}else{
-			system("cls");
-			showHeader();
-			std::cout << "\n\n\n\n\n\n\t\t  "
-					  << "Harap Tunggu\n\n\n\n\n\n\n\n";
-    		sleep(3);
-		}
-	}
-    // ===========================================================
+    masukkanPin(pinT);												// Tampilan masukkan pin
     
     // Main Menu =================================================
     mainMenu(pilihan);												// Tampilan main menu sekaligus menetapkan pilihan transaksi
@@ -88,86 +59,144 @@ int main(){
 			system("cls");
 			break;
 		}
-		case '0':{
+		case '4':{
 			system("cls");
 			break;
 		}
+		case '5':{
+			system("cls");
+			break;
+		}
+		case '6':{
+			system("cls");
+			break;
+		}
+		case '7':{
+			system("cls");
+			break;
+		}
+		case '8':{
+			system("cls");
+			break;
+		}
+		case '0':{
+			keluarKartu();
+			break;
+		}
 	}
-    // ===========================================================
-}
-
-void showHeader(){
-    std::cout << "\tProgram Simulasi Mesin ATM v1.0\n";
+	return 0;
 }
 
 void masukkanKartu(){
 	char ch;
 	system("cls");
-	showHeader();
-    cout << "\n\n\n";
-	cout << "\t Harap Masukkan Kartu ATM Anda\n\n";
-	cout << "     Tekan [Enter] untuk memasukkan kartu\n";
+	cout << "\n\t     SELAMAT DATANG DI\n\n";
+	cout << "\tPROGRAM SIMULASI MESIN ATM\n\n\n\n\n\n\n";
+	cout << "\tSILAHKAN MASUKKAN KARTU ANDA\n"
+		 << "\t      UNTUK PELAYANAN\n\n";
+	cout << "   TEKAN [ENTER] UNTUK MEMASUKKAN KARTU\n\n";
 	ch = _getch();
 	while(ch != 13){
 		ch = _getch();
 	}
-	cout << "\n\n\n\t   Kartu berhasil dimasukkan\n";
-	sleep(3);
+	Beep(600,200);
 }
 
-void masukkanPin(string &pin){
+void masukkanPin(string pinT){
+	string pin;
 	char ch;
-	system("cls");
-	showHeader();
-    cout << "\n\n\n";
-	cout << "\t  Harap Masukkan PIN ATM Anda\n";
-	cout << "\t\t   (6 Digit)\n\n\n";
-	cout << "\t\t     ";
-	while(1){
-		ch = _getch();
-		if(ch == 13 && pin.length() == 6){							// Kondisi untuk keluar loop
-			break;													
+	while(pin != pinT){												// Ketika pin tidak sama dengan pinT
+		pin = "";													// maka akan terus menampilkan tampilan masukkan pin
+		system("cls");
+		cout << "\n\t\tHATI-HATI!!\n"
+			 << "  JANGAN MEMBERITAHUKAN PIN DAN NO KARTU\n"
+			 << "\t   ANDA KEPADA SIAPAPUN\n\n\n\n"
+			 << "      SILAHKAN MASUKKAN NOMOR PIN ANDA\n\n"
+			 << "\t\t  ";
+		while(1){
+			ch = _getch();
+			if(ch == 13 && pin.length() == 6){						// Kondisi untuk keluar loop
+				Beep(600,200);
+				break;													
+			}
+			if(ch == 8 && pin.length() >= 1){						// Menghapus karakter
+				cout << char(8) << " " << char(8);						
+				pin = pin.erase(pin.length()-1);					
+				continue;												
+			}
+			if(ch < 48 || ch > 57 || pin.length() >= 6){			// Hanya angka yang dapat diinput dan 
+				continue;											// ketika sudah 6 karakter tidak bisa input lagi
+			}
+			pin.push_back(ch);
+			cout << "X";
+			Beep(600,200);
 		}
-		if(ch == 8 && pin.length() >= 1){							// Fitur menghapus karakter
-			cout << char(8) << " " << char(8);						
-			pin = pin.erase(pin.length()-1);						
-			continue;												
+		if(pin != pinT){
+    		cout << "\n\n"
+				 << "       PIN YANG ANDA MASUKKAN SALAH\n\n\n\n";
+    		sleep(3);
 		}
-		if(ch < 48 || ch > 57 || pin.length() >= 6){				// Fitur supaya hanya angka yang dapat diinput dan 
-			continue;												// ketika sudah 6 karakter tidak bisa input lagi
-		}
-		pin.push_back(ch);											
-		cout << "*";												
 	}
 }
 
 void mainMenu(char &pilihan){
 	char ch;
 	system("cls");
-	showHeader();
-	cout << "\n\n\n\n";
-	cout << "Pilih transaksi:\n";
-	cout << "[1]Cek Saldo\n"
-		 << "[2]Transfer\n"
-		 << "[3]Pembayaran\n"
-		 << "[0]Cancel\n"
-		 << "\n\n\n\n\nInput: ";
+	cout << "\n\t\tMENU UTAMA\n"
+		 << "\t (PECAHAN UANG RP.50.000)\n"
+		 << "\t==========================\n"
+		 << "\t  TEKAN <0> UNTUK BATAL\n\n"
+	  // << "                                             "
+		 << "[1]<---- 100.000           500.000 ---->[5]\n\n"
+		 << "[2]<---- 300.000           1000.000 --->[6]\n\n"
+		 << "[3]<----- BAYAR/           PENARIKAN -->[7]\n"
+		 << "            BELI           JUMLAH LAIN\n\n"
+		 << "[4]<---- MANDIRI           TRANSAKSI -->[8]\n"
+		 << "        PRABAYAR           LAINNYA\n";
 	while(1){
 		ch = _getch();
-		if(ch == 13 && pilihan != 0){								// Kondisi keluar loop
+		if(ch < 48 || ch > 56){										// Input pilihan hanya boleh angka tertentu
+			continue;
+		}else{
+			pilihan = ch;
+			Beep(600,200);
 			break;
 		}
-		if(ch == 8 && pilihan != 0){								// Fitur untuk menghapus pilihan
-			cout << char(8) << " " << char(8);				
-			pilihan = 0;
-			continue;					
-		}
-		if(ch < 48 || ch > 51 || pilihan != 0){						// Fitur supaya input pilihan hanya boleh angka tertentu
-			continue;												// dan maksimalnya hanya 1 karakter
-		}
-		pilihan = ch;
-		cout << ch;
 	}
+}
+
+void keluarKartu(){
+	char ch;
+	system("cls");
+    cout << "\n\n\n";
+	cout << "\t   Harap Ambil Kartu ATM Anda\n\n";
+	cout << "      Tekan [Enter] untuk mengambil kartu\n";
+	ch = _getch();
+	while(ch != 13){
+		ch = _getch();
+	}
+	system("cls");
+	std::cout << "\n\n\n\n\n\n\n		  Terimakasih\n";
+	sleep(3);
+	system("cls");
+}
+
+void configureCMD(){
+	_COORD coord;													// Variabel untuk menyimpan window size
+	coord.X = 43; 
+	coord.Y = 16;
+	_SMALL_RECT Rect;												// Variabel untuk menyimpan screen buffer size
+    Rect.Top = Rect.Left = 0;
+    Rect.Right = 42;
+	Rect.Bottom = 15;
+	SetConsoleWindowInfo(out, TRUE, &Rect);							// Merubah ukuran window size
+	SetConsoleScreenBufferSize(out, coord);							// Merubah ukuran screen buffer
+	system("title Simulasi Mesin ATM");								// Ubah title CMD
+	fontsize(35, 35);												// Ubah ukuran font pada CMD
+    system("color 1F");												// Merubah warna background CMD menjadi biru & warna teks menjadi putih
+    SetWindowLong(wh, GWL_STYLE,									// Disable resize dan maximize pada window CMD 
+				  GetWindowLong(wh, GWL_STYLE) 
+				  & ~WS_MAXIMIZEBOX & ~WS_SIZEBOX);
 }
 
 void fontsize(int a, int b){  
